340743d981bf9b929bcc46c741ace40a49a7dcaf
Cole McConnell <chm@farmside.co.nz>
2022-12-21 23:31:49 +1300

    implemented grouping posts by day on Queue page

--- src/utils/formatDateTime.ts
+++ src/utils/formatDateTime.ts
@@ -4,7 +4,7 @@ export const formatDateTime = (dateToFormat: string) => {
     const momentDateToFormat = moment(dateToFormat);
     const diff = momentDateToFormat.diff(moment(), "days");
     const timeStr = "h:mma";
-    const dateStr = diff >= 7 ? "dddd, Do MMM" : "dddd";
+    const dateStr = Math.abs(diff) >= 7 ? "dddd, Do MMM" : "dddd";
 
     const formattedTime = momentDateToFormat.format(timeStr);
     const formattedDate = momentDateToFormat.format(dateStr);
