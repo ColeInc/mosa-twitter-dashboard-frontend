02d7c5dac8a9c76d1176370637feb9767da918b8
ColeInc <chm@farmside.co.nz>
2023-02-01 16:20:38 +1300

    reusable dropdown component stable working!

--- src/components/UI/Dropdown.tsx
+++ src/components/UI/Dropdown.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import classes from "./Dropdown.module.scss";
 import RightChevron from "../../assets/icons/noun-right-chevron-4695692.svg";
 
@@ -11,6 +11,21 @@ interface DropdownProps {
 
 const Dropdown: React.FC<DropdownProps> = props => {
     const [dropdownActive, setDropdownActive] = useState(false);
+    const clickOffDropdownRef = useRef<HTMLDivElement>(null);
+
+    // if user clicks outside the opened dropdown menu then close it:
+    const handleClickOutside = (event: MouseEvent) => {
+        if (clickOffDropdownRef.current && !clickOffDropdownRef.current.contains(event.target as Node)) {
+            setDropdownActive(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
 
     const onDropdownClick = (userChoice: string) => {
         // pass the user's choice back up to CreateTweet parent:
@@ -27,6 +42,7 @@ const Dropdown: React.FC<DropdownProps> = props => {
                 event.stopPropagation();
                 setDropdownActive(prev => !prev);
             }}
+            ref={clickOffDropdownRef}
         >
             <button
                 className={`${classes["dropdown__button"]} ${props.className && props.className} ${
@@ -39,16 +55,15 @@ const Dropdown: React.FC<DropdownProps> = props => {
                     <RightChevron />
                 </div>
             </button>
-            <ul className={`${classes["dropdown__items-drawer"]} ${dropdownActive && classes.active}`}>
+            <div className={`${classes["dropdown__items-drawer"]} ${dropdownActive && classes.active}`}>
                 {props.items.map(item => {
                     return (
-                        <React.Fragment key={item}>
-                            <li onClick={onDropdownClick.bind(this, item)}>{item.toUpperCase()}</li>
-                            {/* <hr /> */}
-                        </React.Fragment>
+                        <div onClick={onDropdownClick.bind(this, item)} key={item}>
+                            {item}
+                        </div>
                     );
                 })}
-            </ul>
+            </div>
         </div>
     );
 };
