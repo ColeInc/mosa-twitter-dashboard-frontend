0a99f08141b028949335807716b8efe122d67a09
Cole McConnell <chm@farmside.co.nz>
2023-02-26 20:48:56 +1300

    ultrawide display layout changes, white headings

--- src/components/UI/Layout.tsx
+++ src/components/UI/Layout.tsx
@@ -5,10 +5,9 @@ import classes from "./Layout.module.scss";
 const Layout: React.FC<{ children: ReactElement }> = props => {
     return (
         <main className={classes["layout__container"]}>
+            <NavigationBar />
             <div className={classes["layout__horizontal-container"]}>
-                <NavigationBar />
-
-                {props.children}
+                <div className={classes["layout__content-container"]}>{props.children}</div>
             </div>
         </main>
     );
